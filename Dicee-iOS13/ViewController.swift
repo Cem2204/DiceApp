c0bd65ca49a66a50e5a1245adf53e0a2185b2b15
Cem2204 <cem.cem.strix@gmail.com>
2023-08-02 20:28:46 +0300

    Code Update

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -18,22 +18,20 @@ class ViewController: UIViewController {
     var rightDiceNumber = 2
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceImages = [UIImage(imageLiteralResourceName: "DiceOne"),
+                     UIImage(imageLiteralResourceName: "DiceTwo"),
+                     UIImage(imageLiteralResourceName: "DiceThree"),
+                     UIImage(imageLiteralResourceName: "DiceFour"),
+                     UIImage(imageLiteralResourceName: "DiceFive"),
+                     UIImage(imageLiteralResourceName: "DiceSix")]
+        
         leftDiceNumber = Int.random(in: 0...5)
         rightDiceNumber = Int.random(in: 0...5)
-        diceImageView1.image = [UIImage(imageLiteralResourceName: "DiceOne"),
-         UIImage(imageLiteralResourceName: "DiceTwo"),
-         UIImage(imageLiteralResourceName: "DiceThree"),
-         UIImage(imageLiteralResourceName: "DiceFour"),
-         UIImage(imageLiteralResourceName: "DiceFive"),
-         UIImage(imageLiteralResourceName: "DiceSix")][leftDiceNumber]
+        diceImageView1.image = diceImages[leftDiceNumber]
         
        
-        diceImageView2.image = [UIImage(imageLiteralResourceName: "DiceOne"),
-                                UIImage(imageLiteralResourceName: "DiceTwo"),
-                                UIImage(imageLiteralResourceName: "DiceThree"),
-                                UIImage(imageLiteralResourceName: "DiceFour"),
-                                UIImage(imageLiteralResourceName: "DiceFive"),
-                                UIImage(imageLiteralResourceName: "DiceSix")][rightDiceNumber]
+        diceImageView2.image = diceImages[rightDiceNumber]
         
         
         
