3667863359ab03c52c9e55ce51ff79a2cb8541f4
Cem2204 <cem.cem.strix@gmail.com>
2023-08-04 17:18:29 +0300

    Vertical Display Support

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,9 +13,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    var leftDiceNumber = 1
-    var rightDiceNumber = 2
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
@@ -26,12 +23,8 @@ class ViewController: UIViewController {
                      UIImage(imageLiteralResourceName: "DiceFive"),
                      UIImage(imageLiteralResourceName: "DiceSix")]
         
-        leftDiceNumber = Int.random(in: 0...5)
-        rightDiceNumber = Int.random(in: 0...5)
-        diceImageView1.image = diceImages[leftDiceNumber]
-        
-       
-        diceImageView2.image = diceImages[rightDiceNumber]
+        diceImageView1.image = diceImages[Int.random(in: 0...5)]
+        diceImageView2.image = diceImages[Int.random(in: 0...5)]
         
         
         
